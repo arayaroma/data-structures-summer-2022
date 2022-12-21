ccac7daff3336dd96c27b71260e6581239b53e44
arayaroma <darayaroma@gmail.com>
2022-12-21 16:11:04 -0600

    Fixing Unimplemented Method
    Now I have to implement the methods for this class.

--- clase-3/actividad-1/DoubleLinkedList.cpp
+++ clase-3/actividad-1/DoubleLinkedList.cpp
@@ -8,14 +8,7 @@ DoubleLinkedList::DoubleLinkedList(){
 void DoubleLinkedList::search_letter(std::string word, Node * head){
   char data = word[0];
   Node aux = Node();
-
-  if(head != NULL){
-    //aux = head;
-    //while(aux.get_data() != data && aux){
-
-    //}
-
-  }
+  // Have to implement the methods
 }
 
 bool DoubleLinkedList::is_found(Node* list, Node* queue, char data){
